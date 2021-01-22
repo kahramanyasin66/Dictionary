464fe324fb43957ab67c853b3cc53fec57069d32
yasin kahraman <yasinkahraman66@outlook.com>
2021-01-23 02:13:51 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        public MyDictionary()
+        {
+            keys = new TKey[0];            
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKeys = keys;    
+            TValue[] tempValues = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+            keys[keys.Length - 1] = key;                 
+            values[values.Length - 1] = value;
+        }
+        public int Length
+        {
+            get { return keys.Length; }
+
+        }
+        public TKey[] Keys
+        {
+            get { return keys; }
+        }
+        public TValue[] Values
+        {
+            get { return values; }
+        }
+      
+
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             1) Dictionary sınıfı içinde belirleyeceğimiz bir key'e uygun olarak değer saklar
+             2) key türünü belirleme zorunluluğu var (string ,int ,double)
+             */
+            //Dictionary<string, int> AdYas = new Dictionary<string, int>();
+            //// veri ekleme 
+            //AdYas.Add("Yasin", 25);
+            //AdYas.Add("Bektaş", 41);
+            //AdYas.Add("İlker", 35);
+
+            //foreach (var degeroku in AdYas)
+            //{
+            //    Console.WriteLine(degeroku);
+            //}
+            ////deger sayma 
+            //var eleman = AdYas.Count;
+            //Console.WriteLine("Eleman Sayisi :" + " " + eleman);
+            ////deger silme 
+            //AdYas.Remove("Yasin");
+            //foreach (var degeroku in AdYas)
+            //{
+            //    Console.WriteLine(degeroku);
+            //}
+            //Console.ReadLine();
+
+            MyDictionary<string, int> AdYas = new MyDictionary<string, int>();
+
+            AdYas.Add("Yasin", 25);
+            AdYas.Add("Bektaş", 41);
+            AdYas.Add("İlker", 35);
+
+            foreach (var isim in AdYas.Keys)
+            {
+                Console.WriteLine(isim);
+            }
+            foreach (var yas in AdYas.Values)
+            {
+                Console.WriteLine(yas);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
